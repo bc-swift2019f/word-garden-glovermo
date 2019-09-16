f85e6391414c35ae1f99f8819d916c7bd22c955e
Morgan Glover <glovermo@bc.edu>
2019-09-16 10:37:30 -0400

    3.3 Completed - Single character in TextField, Enable/Disable Guess This Letter Button, Hide Play Again Button

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //print("In viewDidLoad, is guessedLetterField the first responder?", guessedLetterField.isFirstResponder)
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
     }
     
     func updateUIAfterGuess() {
@@ -30,6 +32,13 @@ class ViewController: UIViewController {
     
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
         //print("Hey! The guessedLetterFieldChanged!")
+        if let letterGuessed = guessedLetterField.text?.last {
+            guessedLetterField.text = "\(letterGuessed)"
+            guessLetterButton.isEnabled = true
+        } else {
+            //disable button if no single character in guessedLetterField
+            guessLetterButton.isEnabled = false
+        }
     }
     
     @IBAction func doneKeyPressed(_ sender: UITextField) {
